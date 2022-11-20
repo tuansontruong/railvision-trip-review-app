1593bd65b61b29c0f8de278ed2600a926727b794
tuansontruong <tuansontruongg@gmail.com>
2022-11-20 13:45:08 -0500

    feat(theme): add light and dark mode theme to config

---
+++ src/config/themes.jsx
@@ -0,0 +1,16 @@
+export const themes = {
+  light: {
+    color: '#000000',
+    secondaryColor: '#626E7B',
+    backgroundColor: '#FFFFFF',
+    btnColor: '#B20610',
+    dropdownBackgroundColor: '#F8F9F9'
+  },
+  dark: {
+    color: '#FFFFFF',
+    secondaryColor: '#9d9184',
+    backgroundColor: '#21252E',
+    btnColor: '#CC820E',
+    dropdownBackgroundColor: '#2C2E33'
+  }
+}
