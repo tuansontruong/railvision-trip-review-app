a2d50e9b9d5516772a6530bd29810f4efdcdd160
tuansontruong <tuansontruongg@gmail.com>
2022-11-20 13:54:52 -0500

    feat(theme): add toggle icon to toggle dark-light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
+import styled from 'styled-components'
 import { Routes, Route } from 'react-router-dom'
+import { CgSun } from 'react-icons/cg'
+import { HiMoon } from 'react-icons/hi'
 
 import { ThemeProvider } from 'styled-components'
 
@@ -14,9 +17,39 @@ import { themes } from './config/themes'
 
 import { useLoadTripReviewData } from './hooks/useLoadTripReviewData'
 
-const Page = () => {
+const Toggle = styled.button`
+  position: absolute;
+  top: 20px;
+  right: 40%;
+  cursor: pointer;
+  height: 35px;
+  width: 35px;
+  border-radius: 50%;
+  border: none;
+  &:focus {
+    outline: none;
+  }
+  transition: all 0.5s ease;
+`
+
+const Page = ({ theme, setTheme }) => {
+  const icon =
+    theme === 'light' ? (
+      <HiMoon size={24} />
+    ) : (
+      <CgSun style={{ margin: '4px -3px' }} size={24} />
+    )
   return (
     <>
+      <Toggle
+        onClick={() => {
+          const currrentTheme = theme === 'dark' ? 'light' : 'dark'
+          localStorage.setItem('theme', currrentTheme)
+          setTheme(currrentTheme)
+        }}
+      >
+        {icon}
+      </Toggle>
       <Routes>
         <Route element={<AnimationLayout />}>
           <Route path={PATHS.REVIEW} element={<TripReview />} />
@@ -28,14 +61,14 @@ const Page = () => {
 }
 
 const App = () => {
-  const [theme, setTheme] = useState('light')
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light')
 
   const { tripReviewData } = useLoadTripReviewData()
 
   return (
     <TripReviewContext.Provider value={tripReviewData}>
       <ThemeProvider theme={themes[theme]}>
-        <Page />
+        <Page theme={theme} setTheme={setTheme} />
       </ThemeProvider>
     </TripReviewContext.Provider>
   )
