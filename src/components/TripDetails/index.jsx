0405138bd0f09b7ed5e2235ce2a1189ca779b66b
tuansontruong <tuansontruongg@gmail.com>
2022-11-20 13:11:44 -0500

    feat(tripDetails): implement component TripDetails

---
+++ src/components/TripDetails/index.jsx
@@ -0,0 +1,67 @@
+import {
+  HeaderSecondary,
+  HeaderPrimary,
+  StyledUnoderedList,
+  StyledListItem,
+  StyledListItemSecond
+} from './style'
+
+const TripDetails = ({ trip }) => {
+  return (
+    trip && (
+      <>
+        <HeaderSecondary>Some Date</HeaderSecondary>
+        <HeaderPrimary>
+          TRIP DURATION (todo) &nbsp;|&nbsp; AVG. SPEED{' '}
+          {trip?.averageSpeedInMph} MPH
+        </HeaderPrimary>
+        <div>
+          <StyledUnoderedList>
+            {trip?.tripDetails?.map((station, order) => {
+              const { id, name, arrivedTime, departedTime } = station
+
+              /* First station */
+              if (order === 0)
+                return (
+                  <StyledListItem key={id}>
+                    <img src={require('../../assets/House.png')} alt="start" />
+                    <span>
+                      {id} - {name} | <span>Departed {departedTime}</span>
+                    </span>
+                  </StyledListItem>
+                )
+
+              /* Last station */
+              if (order === trip.tripDetails.length - 1)
+                return (
+                  <StyledListItem key={id}>
+                    <img src={require('../../assets/MapPin.png')} alt="end" />
+                    <span>
+                      {id} - {name} | <span>Arrived {arrivedTime}</span>
+                    </span>
+                  </StyledListItem>
+                )
+
+              return (
+                <StyledListItemSecond key={id}>
+                  <img
+                    src={require('../../assets/GreenCircle.png')}
+                    alt="middle"
+                  />
+                  <span>
+                    {id} - {name} |{' '}
+                    <span>
+                      Arrived {arrivedTime} | Departed {departedTime}
+                    </span>
+                  </span>
+                </StyledListItemSecond>
+              )
+            })}
+          </StyledUnoderedList>
+        </div>
+      </>
+    )
+  )
+}
+
+export default TripDetails
