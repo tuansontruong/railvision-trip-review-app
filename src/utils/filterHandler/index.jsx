2029314415458e7b63400b5ecddb8006cb9fbcd6
tuansontruong <tuansontruongg@gmail.com>
2022-11-20 13:41:35 -0500

    feat(tripFilter): implement logic for each filter

--- src/utils/filterHandler/index.jsx
+++ src/utils/filterHandler/index.jsx
@@ -1,29 +1,71 @@
+import {
+  getTotalTripDuration,
+  getTotalTripDurationInSeconds
+} from '../dateTimeHandler'
+
+// Take the original data object and turn it into a list of objects + additional fields
+export const transformData = tripReviewData => {
+  return Object.entries(tripReviewData).map(tripReviewEntry => {
+    const [tripId, info] = tripReviewEntry
+    const startStationDepartedTime = info.tripDetails[0]?.departedTime
+    const endStationArrivedTime =
+      info.tripDetails[info.tripDetails.length - 1]?.arrivedTime
+
+    // return new object with 3 new fields:
+    // 1.tripId  -  2.durationInSeconds  -  3. numberOfStops
+    return {
+      tripId,
+      durationInSeconds: getTotalTripDurationInSeconds(
+        getTotalTripDuration(startStationDepartedTime, endStationArrivedTime)
+      ),
+      numberOfStops: info.tripDetails.length,
+      ...info
+    }
+  })
+}
+
 export const getTripIdByFilter = (tripReviewData, filter) => {
-  return actionMap[filter](tripReviewData)
+  if (!(tripReviewData && filter)) return null
+
+  const tripReviewDataList = transformData(tripReviewData)
+
+  return actionMap[filter](tripReviewDataList)
 }
 
-export const findLongestDurationTrip = tripReviewData => {
-  return 'A23'
+export const findLongestDurationTrip = tripReviewDataList => {
+  return tripReviewDataList.reduce((prev, current) =>
+    prev.durationInSeconds > current.durationInSeconds ? prev : current
+  ).tripId
 }
 
-export const findShortestDurationTrip = tripReviewData => {
-  return 'A23'
+export const findShortestDurationTrip = tripReviewDataList => {
+  return tripReviewDataList.reduce((prev, current) =>
+    prev.durationInSeconds < current.durationInSeconds ? prev : current
+  ).tripId
 }
 
-export const findHighestSpeedTrip = tripReviewData => {
-  return 'A23'
+export const findHighestSpeedTrip = tripReviewDataList => {
+  return tripReviewDataList.reduce((prev, current) =>
+    prev.averageSpeedInMph > current.averageSpeedInMph ? prev : current
+  ).tripId
 }
 
-export const findLowestSpeedTrip = tripReviewData => {
-  return 'A23'
+export const findLowestSpeedTrip = tripReviewDataList => {
+  return tripReviewDataList.reduce((prev, current) =>
+    prev.averageSpeedInMph < current.averageSpeedInMph ? prev : current
+  ).tripId
 }
 
-export const findMostStopsTrip = tripReviewData => {
-  return 'A23'
+export const findMostStopsTrip = tripReviewDataList => {
+  return tripReviewDataList.reduce((prev, current) =>
+    prev.numberOfStops > current.numberOfStops ? prev : current
+  ).tripId
 }
 
-export const findLeastStopsTrip = tripReviewData => {
-  return 'A23'
+export const findLeastStopsTrip = tripReviewDataList => {
+  return tripReviewDataList.reduce((prev, current) =>
+    prev.numberOfStops < current.numberOfStops ? prev : current
+  ).tripId
 }
 
 const actionMap = {
