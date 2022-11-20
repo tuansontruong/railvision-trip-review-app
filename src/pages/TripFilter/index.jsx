482861782ff09e9a92e6b3bd553b2819df4dc194
tuansontruong <tuansontruongg@gmail.com>
2022-11-20 13:15:30 -0500

    feat(tripFilter): display trip details on trip filter page

--- src/pages/TripFilter/index.jsx
+++ src/pages/TripFilter/index.jsx
@@ -1,12 +1,26 @@
+import { useContext } from 'react'
 import Container from '../../layouts/Container'
 
 import NavigationButton from '../../components/NavigationButton'
 import TripHeader from '../../components/TripHeader'
+import TripDetails from '../../components/TripDetails'
+import { TripReviewContext } from '../../context/tripReviewContext'
+
+import { PATHS } from '../../config/paths'
 
 const TripReview = () => {
+  const tripReviewData = useContext(TripReviewContext)
+  const currentTripId = 'A23'
+  const filteredTrip = tripReviewData[currentTripId]
+
   return (
     <Container>
       <TripHeader text="Trip Filter" />
+      {filteredTrip && (
+        <div key={Math.random()} className="fade-in">
+          <TripDetails trip={filteredTrip} />{' '}
+        </div>
+      )}
       <NavigationButton path={PATHS.REVIEW} btnText={'Go to Trip Review'} />
     </Container>
   )
