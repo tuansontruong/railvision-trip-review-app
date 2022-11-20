a07e9a0de38960a06d53ecc0bee3e10193685395
tuansontruong <tuansontruongg@gmail.com>
2022-11-20 14:11:33 -0500

    test(page): add incomplete test files for pages

---
+++ src/pages/TripFilter/.test.jsx
@@ -0,0 +1,9 @@
+// Did not write all the component tests
+// Refer to TripHeader / .test.jsx as example
+
+import { render, screen } from '@testing-library/react'
+import NavigationButton from '.'
+
+test('Test', () => {
+  // to complete
+})
