62ada7f640b387be36905edce77da2a3ff593d16
tuansontruong <tuansontruongg@gmail.com>
2022-11-20 12:59:58 -0500

    chore: add custom hook for api fetch trip review data

---
+++ src/hooks/useLoadTripReviewData.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react'
+import { fetchTripReviewData } from '../api/mockAPI'
+
+export const useLoadTripReviewData = () => {
+  const [tripReviewData, setTripReviewData] = useState(null)
+
+  useEffect(() => {
+    // API fetch on page load
+    fetchTripReviewData().then(data => setTripReviewData(data))
+  }, [tripReviewData])
+
+  return { tripReviewData }
+}
