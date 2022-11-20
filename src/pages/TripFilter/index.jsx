197f72f3812dfdd66667e6fce1080b8501d343fa
tuansontruong <tuansontruongg@gmail.com>
2022-11-20 12:44:36 -0500

    feat: display headers and navigation buttons in 2 pages

--- src/pages/TripFilter/index.jsx
+++ src/pages/TripFilter/index.jsx
@@ -1,5 +1,15 @@
-const TripFilter = () => {
-  return <></>;
-};
+import Container from '../../layouts/Container'
 
-export default TripFilter;
+import NavigationButton from '../../components/NavigationButton'
+import TripHeader from '../../components/TripHeader'
+
+const TripReview = () => {
+  return (
+    <Container>
+      <TripHeader text="Trip Filter" />
+      <NavigationButton path={PATHS.REVIEW} btnText={'Go to Trip Review'} />
+    </Container>
+  )
+}
+
+export default TripReview
--- src/pages/TripReview/index.jsx
+++ src/pages/TripReview/index.jsx
@@ -1,5 +1,15 @@
-const TripReview = () => {
-  return <></>;
-};
+import Container from '../../layouts/Container'
 
-export default TripReview;
+import NavigationButton from '../../components/NavigationButton'
+import TripHeader from '../../components/TripHeader'
+
+const TripFilter = () => {
+  return (
+    <Container>
+      <TripHeader text="Trip Review" />
+      <NavigationButton path={PATHS.FILTER} btnText={'Go to Trip Filter'} />
+    </Container>
+  )
+}
+
+export default TripFilter
