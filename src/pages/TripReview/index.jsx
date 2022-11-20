9f518a215b429c2bc76662322e37defa3e2040af
tuansontruong <tuansontruongg@gmail.com>
2022-11-20 13:12:13 -0500

    feat(tripReview): display trip details

--- src/pages/TripReview/index.jsx
+++ src/pages/TripReview/index.jsx
@@ -1,12 +1,25 @@
+import { useContext } from 'react'
 import Container from '../../layouts/Container'
+import { TripReviewContext } from '../../context/tripReviewContext'
 
 import NavigationButton from '../../components/NavigationButton'
 import TripHeader from '../../components/TripHeader'
+import TripDetails from '../../components/TripDetails'
+
+import { PATHS } from '../../config/paths'
 
 const TripFilter = () => {
+  const tripReviewData = useContext(TripReviewContext)
+  const currentTrip = tripReviewData && 'A23'
+
   return (
     <Container>
       <TripHeader text="Trip Review" />
+      {currentTrip && (
+        <div key={Math.random()}>
+          <TripDetails trip={currentTrip} />{' '}
+        </div>
+      )}
       <NavigationButton path={PATHS.FILTER} btnText={'Go to Trip Filter'} />
     </Container>
   )
